a4b4b3f0c8dc3f1b7286c11cdc336d35db2fb94d
AlexZodiac13 <zodiac1993@ya.ru>
2022-12-20 10:16:10 +0300

    commit v0.1

---
+++ main.py
@@ -0,0 +1,9 @@
+import telebot
+bot = telebot.TeleBot('745192957:AAE1I3Mi-p0P0mdOj1sQoTht9qneP01BAxk')
+commit = open("commit.txt",'r').read()
+bot.send_message(410461386, commit)
+@bot.message_handler(commands=['start', 'help'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет')
+    print(message.chat.id)
+
