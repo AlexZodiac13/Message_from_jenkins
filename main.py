22cc5e4fa649048c92ae75a94398d3450b93a91c
AlexZodiac13 <zodiac1993@ya.ru>
2022-12-21 14:46:25 +0300

    commit v0.13

--- main.py
+++ main.py
@@ -4,6 +4,5 @@ commit = open("commit.txt",'r').read()
 bot.send_message(410461386, commit)
 @bot.message_handler(commands=['start', 'help'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет')
+    bot.send_message(message.chat.id, 'Привввет')
     print(message.chat.id)
-
